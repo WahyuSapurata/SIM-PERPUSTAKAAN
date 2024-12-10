263716fcaddbf590f51038ff65c35c92a09ba31f
WahyuSapurata <wm337708@gmail.com>
2024-12-11 06:32:14 +0800

    update Dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -38,9 +38,10 @@ class Dashboard extends BaseController
     public function areaChart()
     {
         // Mendapatkan data dari database menggunakan query builder Laravel
-        $data = Peminjaman::selectRaw('COUNT(id) as count, TO_CHAR(created_at, \'Month\') as month_name')
+        $data = Peminjaman::selectRaw('COUNT(id) as count, DATE_FORMAT(created_at, \'%M\') as month_name')
             ->whereYear('created_at', '=', date('Y'))
-            ->groupByRaw('TO_CHAR(created_at, \'Month\')')
+            ->groupByRaw('DATE_FORMAT(created_at, \'%M\')')
+            ->orderByRaw('MIN(created_at)') // Agar urutan bulan sesuai waktu
             ->get();
 
         // Inisialisasi array data
